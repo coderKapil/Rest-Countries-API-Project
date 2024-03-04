98b7e7b077aa820cd64725d57342e80d403d7d3a
Kapil Rana <kapsrana56@gmail.com>
2024-03-04 10:37:47 +0530

    back button functionality added, and country info page and border country with their functionality added into project

--- script.js
+++ script.js
@@ -4,18 +4,17 @@ fetch("https://restcountries.com/v3.1/all")
     .then((resp) => resp.json())
     .then((data) => {
         data.forEach((country) => {
-            // console.log(country.flags.svg);
+            console.log(country.region);
             const countryCard = document.createElement("a");
             countryCard.classList.add("country-card");
             countryCard.innerHTML = `
-<img src="${country.flags.svg}" alt="flag" />
-<div class="card-text">
-  <h3 class="card-title">Vatican City</h3>
-  <p><b>Population: </b>81,770310</p>
-  <p><b>Region: </b>Europe</p>
-  <p><b>Capital: </b>Berlin</p>
-</div>
-`;
+            <img src="${country.flags.svg}" alt="flag" />
+             <div class="card-text">
+            <h3 class="card-title">${country.name.common}</h3>
+           <p><b>Population: </b>${country.population}</p>
+           <p><b>Region: </b>${country.region}</p>
+            <p><b>Capital: </b>${country.capital}</p>
+          </div>`
             countriesContainer.append(countryCard);
         });
     });
